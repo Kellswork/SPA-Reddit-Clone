4f7dcffb3f4b0d580c9d11f0c80cb85bff9be5b0
kells <kellogbonna@gmail.com>
2020-07-31 03:15:42 +0100

    add styles for home page

--- src/components/SubredditPost.jsx
+++ src/components/SubredditPost.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { ContentCard } from "./~styled";
-import moment from 'moment'
+import moment from "moment";
+import { ReactTinyLink } from "react-tiny-link";
 
 function kFormatter(num) {
   return Math.abs(num) > 999
@@ -9,23 +10,35 @@ function kFormatter(num) {
     : Math.sign(num) * Math.abs(num);
 }
 
-
 function SubredditPost(props) {
-    const postTime = moment.unix(props.post.data.created_utc).fromNow();
+  const postTime = moment.unix(props.post.data.created_utc).fromNow();
+
   return (
     <ContentCard>
       <div className="upvotes">
         <p>{kFormatter(props.post.data.ups)}</p>
       </div>
-      <div className='content-right'>
-        <div className='card-top'>
-          <p className='sub-heading'>{props.post.data.subreddit_name_prefixed}</p>
+      <div className="content-right">
+        <div className="card-top">
+          <p className="sub-heading">
+            {props.post.data.subreddit_name_prefixed}
+          </p>
           <p>Posted by u/{props.post.data.author}</p>
           <p>{postTime}</p>
         </div>
-        <div className='post-details'>
+        <div className="post-details">
           <h3>{props.post.data.title}</h3>
-          <p>{props.post.data.selftext}</p>
+          {/* <p>{props.post.data.url}</p> */}
+          <div className='link'>
+          <ReactTinyLink
+            cardSize="small"
+            showGraphic={true}
+            maxLine={2}
+            minLine={1}
+            url={props.post.data.url}
+          />
+          </div>
+          
         </div>
       </div>
     </ContentCard>
--- src/components/~styled/index.js
+++ src/components/~styled/index.js
@@ -4,14 +4,14 @@ export const MainContainer = styled.div`
   background-color: #dae0e6;
   width: 100%;
   padding-top: 20px;
-  font-family: 'Inter', sans-serif;
+  font-family: "Inter", sans-serif;
 `;
 
 export const ContentCard = styled.div`
   background-color: white;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   width: 600px;
-  height: 400px;
+  /* height: 300px; */
   margin: 0 auto;
   margin-bottom: 20px;
   border-radius: 4px;
@@ -26,33 +26,50 @@ export const ContentCard = styled.div`
       font-weight: bold;
     }
   }
-  .content-right{
+  .content-right {
+    box-sizing: border-box;
     padding-left: 20px;
     padding-right: 20px;
+    padding-bottom: 20px;
   }
   .card-top {
     display: flex;
     p {
-        padding-right: 10px;
-        font-size: 0.8rem;
-        color: #C1C3C4;
-        padding-bottom: 10px;
-        padding-top: 10px;
-        
+      padding-right: 10px;
+      font-size: 0.8rem;
+      color: #c1c3c4;
+      padding-bottom: 10px;
+      padding-top: 10px;
     }
     p:first-child {
-        color: black;
-        font-weight: bold;
+      color: black;
+      font-weight: bold;
     }
   }
   .sub-heading {
-      font-weight: bold;
+    font-weight: bold;
   }
   .post-details {
-      padding-top: 10px;
-      h3 {
-          font-weight: bold;
-          font-size: 1.1rem;
-      }
+    h3 {
+      font-weight: bold;
+      font-size: 1.1rem;
+      padding-bottom: 10px;
+    }
+    p {
+      font-size: 0.9rem;
+      color: grey;
+    }
+    .react_tinylink_card {
+      background-color: inherit;
+      border-width: inherit;
+      border-style: none;
+      box-shadow: none;
+    }
+    .react_tinylink_card_content_wrapper {
+      display: none;
+    }
+    .react_tinylink_card_media {
+      border-radius: 2px;
+    }
   }
 `;
