8c064ec92f9ed437fb364c6edf4cb04a4c2923f0
kells <kellogbonna@gmail.com>
2020-07-31 03:15:42 +0100

    add styles for home page

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div>
+            <div className='logo'></div>
+            <div className='filter-box'></div>
+        </div>
+    )
+}
