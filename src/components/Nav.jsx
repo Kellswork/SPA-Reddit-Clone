a267c693ca75083cc0b0078785c16bf05a8995a4
kells <kellogbonna@gmail.com>
2020-07-31 10:47:58 +0100

    implement search functionality

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavContainer } from "./~styled/Nav";
+import { connect } from "react-redux";
 
-export default function Nav() {
+
+function Nav({ posts }) {
+  const [input, updateInput] = useState("");
+
+  const handleChange = (event) => {
+    updateInput({ input: event.target.value });
+  };
+
   return (
     <NavContainer>
       <div className="search-box">
         <label htmlFor="search">
-          <input type="text"  placeholder='Search'/>
+          <input
+            type="text"
+            name="search"
+            placeholder="Search"
+            onChange={handleChange}
+          />
         </label>
       </div>
     </NavContainer>
   );
 }
+const mapStateToProps = (state) => ({
+  posts: state.posts.posts,
+});
+
+export default connect(mapStateToProps, {})(Nav);
