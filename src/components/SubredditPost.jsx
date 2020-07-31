6def174fc6a4b5b49951af21e61faa7dc1a5b010
kells <kellogbonna@gmail.com>
2020-07-31 07:50:25 +0100

    include should have props

--- src/components/SubredditPost.jsx
+++ src/components/SubredditPost.jsx
@@ -10,9 +10,22 @@ function kFormatter(num) {
     : Math.sign(num) * Math.abs(num);
 }
 
+const LinkIcon = () => (
+  <svg
+    fill="#000000"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="20px"
+    height="20px"
+  >
+    <path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z" />
+  </svg>
+);
+
 function SubredditPost(props) {
   const postTime = moment.unix(props.post.data.created_utc).fromNow();
-
+  const urlLink = `https://www.reddit.com${props.post.data.permalink}`;
+  console.log(urlLink);
   return (
     <ContentCard>
       <div className="upvotes">
@@ -29,16 +42,21 @@ function SubredditPost(props) {
         <div className="post-details">
           <h3>{props.post.data.title}</h3>
           {/* <p>{props.post.data.url}</p> */}
-          <div className='link'>
-          <ReactTinyLink
-            cardSize="small"
-            showGraphic={true}
-            maxLine={2}
-            minLine={1}
-            url={props.post.data.url}
-          />
+          <div className="post-link">
+            <ReactTinyLink
+              cardSize="small"
+              showGraphic={true}
+              maxLine={2}
+              minLine={1}
+              url={props.post.data.url}
+            />
+            <a class='reddit-link' href={urlLink} target="_blank" rel="noopener noreferrer">
+              link to post
+              <span>
+                <LinkIcon />
+              </span>
+            </a>
           </div>
-          
         </div>
       </div>
     </ContentCard>
